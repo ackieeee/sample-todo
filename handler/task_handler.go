ae150102fda0e3e30d5e0da336bb24dc2c0cdf0c
gba-3 <gbayru3@gmail.com>
2022-04-17 00:45:50 +0900

    handler 修正

--- handler/task_handler.go
+++ handler/task_handler.go
@@ -3,8 +3,8 @@ package handler
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"io/ioutil"
-	"log"
 	"net/http"
 
 	"github.com/gba-3/sample-todo/domain/entity"
@@ -16,75 +16,59 @@ type taskhandler struct {
 }
 
 type TaskHandler interface {
-	ChangeStatus(w http.ResponseWriter, r *http.Request)
-	AddTask(w http.ResponseWriter, r *http.Request)
-	GetAll(w http.ResponseWriter, r *http.Request)
+	ChangeStatus(w http.ResponseWriter, r *http.Request) (int, interface{}, error)
+	AddTask(w http.ResponseWriter, r *http.Request) (int, interface{}, error)
+	GetAll(w http.ResponseWriter, r *http.Request) (int, interface{}, error)
 }
 
 func NewTaskHandler(tu usecase.TaskUsecase) TaskHandler {
 	return &taskhandler{tu}
 }
 
-func (th *taskhandler) ChangeStatus(w http.ResponseWriter, r *http.Request) {
+func (th *taskhandler) ChangeStatus(w http.ResponseWriter, r *http.Request) (int, interface{}, error) {
 	ctx := context.Background()
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		w.Write([]byte(err.Error()))
-		return
+		return http.StatusBadRequest, nil, err
 	}
 
 	task := entity.Task{}
 	if err := json.Unmarshal(body, &task); err != nil {
-		w.Write([]byte(err.Error()))
-		return
+		return http.StatusBadRequest, nil, err
 	}
 	if task.ID == 0 {
-		w.Write([]byte("task id is empty"))
-		return
+		return http.StatusBadRequest, nil, errors.New("task id is empty.")
 	}
 	err = th.tu.ChangeStatus(ctx, task.ID, task.Status)
 	if err != nil {
-		w.Write([]byte(err.Error()))
-		return
+		return http.StatusBadRequest, nil, err
 	}
-	w.Write([]byte("success: ChangeStatus"))
+	return http.StatusOK, "success", nil
 }
 
-func (th *taskhandler) AddTask(w http.ResponseWriter, r *http.Request) {
+func (th *taskhandler) AddTask(w http.ResponseWriter, r *http.Request) (int, interface{}, error) {
 	ctx := context.Background()
 
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		w.Write([]byte(err.Error()))
-		return
+		return http.StatusBadRequest, nil, err
 	}
 
 	task := entity.Task{}
 	if err := json.Unmarshal(body, &task); err != nil {
-		w.Write([]byte(err.Error()))
-		return
+		return http.StatusBadRequest, nil, err
 	}
 	if err := th.tu.AddTask(ctx, task.Title, task.Description, task.Date); err != nil {
-		w.Write([]byte(err.Error()))
-		return
+		return http.StatusBadRequest, nil, err
 	}
-	w.Write([]byte("success: add task."))
-
+	return http.StatusOK, "success", nil
 }
 
-func (th *taskhandler) GetAll(w http.ResponseWriter, r *http.Request) {
+func (th *taskhandler) GetAll(w http.ResponseWriter, r *http.Request) (int, interface{}, error) {
 	ctx := context.Background()
 	tasks, err := th.tu.GetAll(ctx)
 	if err != nil {
-		log.Println(err.Error())
-		w.Write([]byte("failed get all tasks."))
-		return
+		return http.StatusBadRequest, nil, err
 	}
-	jsonData, err := tasks.ToJson()
-	if err != nil {
-		log.Println(err.Error())
-		w.Write([]byte("failed convert to json."))
-		return
-	}
-	w.Write(jsonData)
+	return http.StatusOK, tasks, nil
 }
